2ca5c21b55e7691f0f9f624a5d27d3f5324d60a0
praveenb <praveeb@proarch.com>
2020-06-02 02:22:09 +0530

    US-501 created proarch-ems instead of pages

---
+++ src/app/proarch-ems/time-sheet/table/table.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+import { SmartTableData } from '../../../@core/data/smart-table';
+@Component({
+  selector: 'ngx-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent {
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      firstName: {
+        title: 'First Name',
+        type: 'string',
+      },
+      lastName: {
+        title: 'Last Name',
+        type: 'string',
+      },
+      username: {
+        title: 'Username',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      age: {
+        title: 'Age',
+        type: 'number',
+      },
+      phone: {
+        title: 'Phone',
+        type: 'string'
+      },
+      gender: {
+        title: 'Gender',
+        type: 'string'
+      }
+
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private service: SmartTableData) {
+    const data = this.service.getData();
+    this.source.load(data);
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+}
--- src/app/proarch-ems/time-sheet/time-sheet-routing.module.ts
+++ src/app/proarch-ems/time-sheet/time-sheet-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TimeSheetComponent } from './time-sheet.component';
 import { SmartTableComponent } from '../../pages/tables/smart-table/smart-table.component';
+import { TableComponent } from './table/table.component';
 
 
 const routes: Routes = [
@@ -10,8 +11,8 @@ const routes: Routes = [
     component: TimeSheetComponent,
     children: [
       {
-        path: 'smart-table',
-        component: SmartTableComponent,
+        path: 'table',
+        component: TableComponent,
       },
 
     ]
